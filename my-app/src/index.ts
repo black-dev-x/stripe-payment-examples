c44ada4a544bf7907e5ff29e8ef6f8f9c8875d8c
black-dev-x <thiago.dev.x@gmail.com>
2024-06-27 12:31:02 -0300

    post request

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -6,6 +6,9 @@ const app = new Hono()
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+app.post('/', c => {
+  return c.text('Post Request from Hono!')
+})
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
