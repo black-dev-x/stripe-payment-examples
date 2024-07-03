71c635e6920f508328f0f66e6186ad4b89f8b987
black-dev-x <thiago.dev.x@gmail.com>
2024-07-03 16:31:10 -0300

    update

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -2,6 +2,7 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import Stripe from 'stripe'
 import "dotenv/config"
+import { HTTPException } from 'hono/http-exception'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
@@ -12,6 +13,35 @@ app.get('/', (c) => {
 })
 app.post('/', c => {
   return c.text('Post Request from Hono!')
+})
+app.post('/checkout', async (c) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: 'price_this_is_a_price_id_from_the_dashboard',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3000/success',
+      cancel_url: 'http://localhost:3000/cancel',
+    })
+    // the json has a lot of information, but the most important thing, is actually a url
+    // that we can redirect the user to, to complete the payment
+    return c.json(session)
+  } catch(error: any) {
+    throw new HTTPException(500, { message: error.message })
+  }
+})
+
+app.get('/success', (c) => {
+  return c.text('Payment Successful!')
+})
+
+app.get('/cancel', (c) => {
+  return c.text('Payment Cancelled!')
 })
 
 const port = 3000
